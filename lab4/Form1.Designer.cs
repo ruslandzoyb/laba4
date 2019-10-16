972beda3f386bff7d5a9ea83bd3dd15ff02099a8
rusya <dnepr.ukrainec@gmail.com>
2019-10-16 16:03:24 +0300

    Fix

--- lab4/Form1.Designer.cs
+++ lab4/Form1.Designer.cs
@@ -63,6 +63,7 @@
             this.checkBox1.TabIndex = 1;
             this.checkBox1.Text = "1";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // checkBox2
             // 
@@ -73,6 +74,7 @@
             this.checkBox2.TabIndex = 2;
             this.checkBox2.Text = "2";
             this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // checkBox3
             // 
@@ -83,6 +85,7 @@
             this.checkBox3.TabIndex = 3;
             this.checkBox3.Text = "3";
             this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
             // 
             // checkBox4
             // 
@@ -93,6 +96,7 @@
             this.checkBox4.TabIndex = 4;
             this.checkBox4.Text = "4";
             this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
             // 
             // checkBox5
             // 
@@ -103,6 +107,7 @@
             this.checkBox5.TabIndex = 5;
             this.checkBox5.Text = "5";
             this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
             // 
             // checkBox6
             // 
@@ -113,6 +118,7 @@
             this.checkBox6.TabIndex = 6;
             this.checkBox6.Text = "6";
             this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
             // 
             // checkBox7
             // 
@@ -123,6 +129,7 @@
             this.checkBox7.TabIndex = 7;
             this.checkBox7.Text = "7";
             this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
             // 
             // checkBox8
             // 
@@ -133,6 +140,7 @@
             this.checkBox8.TabIndex = 8;
             this.checkBox8.Text = "8";
             this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
             // 
             // textBox1
             // 
@@ -148,6 +156,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
@@ -155,6 +164,7 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 22);
             this.textBox3.TabIndex = 11;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label2
             // 
@@ -178,10 +188,11 @@
             // 
             this.button1.Location = new System.Drawing.Point(260, 472);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.Size = new System.Drawing.Size(110, 37);
             this.button1.TabIndex = 14;
             this.button1.Text = "Вычислить";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -204,7 +215,7 @@
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Лабораторна № 4 Дзьобко Руслан";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -15,11 +15,197 @@ namespace lab4
         public Form1()
         {
             InitializeComponent();
+            
         }
+        int processors = Environment.ProcessorCount;
+        int checkedProc;
+        int arrayLength;
+        int threadCount;
+        List<CheckBox> boxes = new List<CheckBox>();
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            label1.Text += " "+processors.ToString();
+            //  MessageBox.Show(processors.ToString());
+            foreach (Control item in this.Controls)
+            {
+                if (item is CheckBox )
+                {
+                    boxes.Add((CheckBox)item);
+                }
+            }
+            boxes.Reverse();
+            for (int i = boxes.Count-1; i >=0; i--)
+            {
+                if (i<processors)
+                {
+                    boxes[i].Enabled = true;
+                }
+                else
+                {
+                    boxes[i].Enabled = false;
+                }
+                
+            }
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(checkedProc.ToString());
+        }
+
+        private void CheckPrevious(int k)
+        {
+            if (k>0)
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    if (!boxes[i].Checked)
+                    {
+                        boxes[i].Checked = true;
+
+
+                    }
+
+
+                }
+            }
+            
+            
+            checkedProc = ++k;
+            
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                CheckPrevious(0);
+            }
+            else
+            {
+
+            }
+           
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                CheckPrevious(1);
+            }
+            else
+            {
+                CheckPrevious(0);
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                CheckPrevious(2);
+            }
+            else
+            {
+                CheckPrevious(1);
+            }
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                CheckPrevious(3);
+            }
+            else
+            {
+                CheckPrevious(2);
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox5.Checked)
+            {
+                CheckPrevious(4);
+            }
+            else
+            {
+                CheckPrevious(3);
+            }
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox6.Checked)
+            {
+                CheckPrevious(5);
+            }
+            else
+            {
+                CheckPrevious(4);
+            }
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox7.Checked)
+            {
+                CheckPrevious(6);
+            }
+            else
+            {
+                CheckPrevious(5);
+            }
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox8.Checked)
+            {
+                CheckPrevious(7);
+            }
+            else
+            {
+                CheckPrevious(6);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+            if (textBox2.Text == "-"|| textBox2.Text=="0")
+            {
+                throw new Exception("Только положительные числа");
+            }
+            if (textBox2.Text!="")
+            {
+                arrayLength = Convert.ToInt32(textBox2.Text);
+            }
+            
+           
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "-"|| textBox3.Text == "0")
+            {
+                throw new Exception("Только положительные числа");
+            }
+            if (textBox3.Text!="")
+            {
+                threadCount = Convert.ToInt32(textBox3.Text);
+                
+            }
+           
+                
+            
+           
+
+            
         }
     }
 }
